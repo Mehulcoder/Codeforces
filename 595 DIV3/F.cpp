1eda398c0f55a0028edc0b0563b68bc08566ef19
Mehulcoder <mehul355180@gmail.com>
2019-10-24 06:02:44 +0530

    Added more problems

--- 595 DIV3/F.cpp
+++ 595 DIV3/F.cpp
@@ -35,12 +35,27 @@ typedef unordered_set<int> useti;
 #define MOD 1000000007
 
 
+
 int main( int argc , char ** argv )
 {
 	ios_base::sync_with_stdio(false) ; 
 	cin.tie(NULL) ; 
 	
-	
+	int n, k;
+	cin>>n>>k;
+
+	vector<int> weight(n, 0);
+	vector<int>* edges = new vector<int>[n-1];
+
+	for (int i = 0; i < n-1; ++i)
+	{
+		int a, b;
+		cin>>a>>b;
+		edges[a-1].push_back(b-1);
+		edges[b-1].push_back(a-1);
+	}
+
+	cout << go(0, weight, edges, n, k) << '\n';
 
 
 
