4fbe809a0556ddd8768c11174bd638a9065d8f28
Mehulcoder <mehul355180@gmail.com>
2019-10-31 00:42:36 +0530

    Added more cool stuff

--- Graph Practice/1241D.cpp
+++ Graph Practice/1241D.cpp
@@ -33,14 +33,37 @@ typedef unordered_set<int> useti;
 #define f first
 #define s second
 #define MOD 1000000007
-
-
+const int MAXN = 1000000+3;
+ 
+int n, m;
+string a[MAXN];
+ 
+bool rec(int x, int y){
+    if(x == n-1 && y == m-1) return 1;
+    if(x >= n || y >= m || a[x][y] == '#') return 0;
+    if(x != 0 || y != 0) a[x][y] = '#';
+    return rec(x + 1, y) || rec(x, y + 1);
+}
+ 
 int main( int argc , char ** argv )
 {
 	ios_base::sync_with_stdio(false) ; 
 	cin.tie(NULL) ; 
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+	#endif
+
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    if(!rec(0,0)) cout << 0;
+    else if(rec(0,0)) cout << 2;
+    else cout << 1;
 	
-	
+
 
 
 	return 0 ; 
@@ -48,3 +71,4 @@ int main( int argc , char ** argv )
 
 
 }
+
