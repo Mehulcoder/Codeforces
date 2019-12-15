fb0fde5c3a2b07469993729dacb6f238d44fc5be
Mehulcoder <mehul355180@gmail.com>
2019-12-15 13:54:20 +0530

    607 DIV 2

--- Techno/D.cpp
+++ Techno/D.cpp
@@ -41,7 +41,91 @@ int main( int argc , char ** argv )
 	ios_base::sync_with_stdio(false) ; 
 	cin.tie(NULL) ; 
 	
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		unordered_map<string, int> m1;
 
+		for (int i = 0; i < n; ++i)
+		{
+			string s;
+			cin>>s;
+
+			if (s[0]=='1' && s[s.size()-1]=='1')
+			{
+				m1["11"]++;
+			}
+			if (s[0]=='1' && s[s.size()-1]=='0')
+			{
+				m1["10"]++;
+			}
+			if (s[0]=='0' && s[s.size()-1]=='1')
+			{
+				m1["01"]++;
+			}
+			if (s[0]=='0' && s[s.size()-1]=='0')
+			{
+				m1["00"]++;
+			}
+
+			// DEBUG(s)
+			// DEBUG(m1["01"])
+		}
+
+		if (m1["01"]==0 && m1["10"]==0 && m1["00"]!=0 && m1["11"]!=0)
+		{
+			cout << -1 << '\n';
+			continue;
+		}
+
+		int ans = 0;
+
+//Used at start
+		if (m1["10"]>=2 && m1["11"]!=0)
+		{
+			m1["10"]-=2;
+		}else if (m1["11"]==0)
+		{
+			int temp = min(m1["10"], m1["01"]);
+			m1["10"] -= temp;
+			m1["01"] -= temp;
+			ans = max(m1["01"], m1["10"])/2;
+			cout<<ans<<endl;
+			continue;
+		}
+
+		int temp = min(m1["10"], m1["01"]);
+		m1["10"] -= temp;
+		m1["01"] -= temp;
+//Used in last
+		if (m1["10"]==0)
+		{			
+			ans = m1["01"]/2;
+			cout << ans << '\n';
+			continue;
+		}else{
+			ans = (m1["10"])/2+1;
+
+			cout << ans << '\n';
+			continue;
+		}
+
+//oo consumed
+		m1["00"] = 0;
+
+// start consuming pairs of 01 and 10
+
+		
+
+		// for(auto it: m1){
+		// 	cout << it.first << " : "<<it.second;
+		// 	cout <<  '\n';
+		// }
+
+
+	}
 	
 
 
