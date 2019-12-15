8f4c04ed3762361db08c4bfc0cdc7be5e35bff00
Mehulcoder <mehul355180@gmail.com>
2019-12-16 00:27:07 +0530

    Codeforces 608DIV2. It was a pretty nice one.

--- Techno/D.cpp
+++ Techno/D.cpp
@@ -1,138 +1,127 @@
-/*
-
-				Name: Mehul Chaturvedi
-				IIT-Guwahati
-
-*/
-
-/*
-				PROBLEM STATEMENT
-
-*/
-
+#pragma GCC optimize("O3")
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef unordered_map<int, int> umapii;
-typedef unordered_map<int, bool> umapib;
-typedef unordered_map<string, int> umapsi;
-typedef unordered_map<string, string> umapss;
-typedef map<string, int> mapsi;
-typedef map<pair<int, int>, int> mappiii;
-typedef map<int, int> mapii;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef unordered_set<int> useti;
-
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define uset unordered_set
-#define it iterator
+ 
+#define ll long long
 #define mp make_pair
+#define pi pair<int,int>
+#define pl pair<ll,ll>
+#define ppi pair<int,pi >
+#define ppl pair<ll,pl >
+#define ld long double
 #define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define f first
-#define s second
-#define MOD 1000000007
-
-
-int main( int argc , char ** argv )
+#define mod 1000000007
+ 
+int main()
 {
-	ios_base::sync_with_stdio(false) ; 
-	cin.tie(NULL) ; 
-	
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		unordered_map<string, int> m1;
-
-		for (int i = 0; i < n; ++i)
-		{
-			string s;
-			cin>>s;
-
-			if (s[0]=='1' && s[s.size()-1]=='1')
-			{
-				m1["11"]++;
-			}
-			if (s[0]=='1' && s[s.size()-1]=='0')
-			{
-				m1["10"]++;
-			}
-			if (s[0]=='0' && s[s.size()-1]=='1')
-			{
-				m1["01"]++;
-			}
-			if (s[0]=='0' && s[s.size()-1]=='0')
-			{
-				m1["00"]++;
-			}
-
-			// DEBUG(s)
-			// DEBUG(m1["01"])
-		}
-
-		if (m1["01"]==0 && m1["10"]==0 && m1["00"]!=0 && m1["11"]!=0)
-		{
-			cout << -1 << '\n';
-			continue;
-		}
-
-		int ans = 0;
-
-//Used at start
-		if (m1["10"]>=2 && m1["11"]!=0)
-		{
-			m1["10"]-=2;
-		}else if (m1["11"]==0)
-		{
-			int temp = min(m1["10"], m1["01"]);
-			m1["10"] -= temp;
-			m1["01"] -= temp;
-			ans = max(m1["01"], m1["10"])/2;
-			cout<<ans<<endl;
-			continue;
-		}
-
-		int temp = min(m1["10"], m1["01"]);
-		m1["10"] -= temp;
-		m1["01"] -= temp;
-//Used in last
-		if (m1["10"]==0)
-		{			
-			ans = m1["01"]/2;
-			cout << ans << '\n';
-			continue;
-		}else{
-			ans = (m1["10"])/2+1;
-
-			cout << ans << '\n';
-			continue;
-		}
-
-//oo consumed
-		m1["00"] = 0;
-
-// start consuming pairs of 01 and 10
-
-		
-
-		// for(auto it: m1){
-		// 	cout << it.first << " : "<<it.second;
-		// 	cout <<  '\n';
-		// }
-
-
-	}
-	
-
-
-
-	return 0 ; 
-
-
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        int flag1=0,flag0=0,flag=0;
+        vector<int> start0;
+        vector<int> start1;
+        int count0=0,count1=0;
+        unordered_set<string> s;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            s.insert(arr[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            int m=arr[i].size();
+            if(arr[i][0]=='0' && arr[i][m-1]=='0')
+                flag0=1;
+            else if(arr[i][0]=='1' && arr[i][m-1]=='1')
+                flag1=1;
+            else
+            {
+                if(arr[i][0]=='0')
+                {
+                    count0++;
+                    string lol=arr[i];
+                    reverse(lol.begin(),lol.end());
+                    if(s.find(lol)==s.end())
+                        start0.pb(i);
+                }
+                else
+                {
+                    count1++;
+                    string lol=arr[i];
+                    reverse(lol.begin(),lol.end());
+                    if(s.find(lol)==s.end())
+                        start1.pb(i);
+                }
+                flag=1;
+            }
+        }
+        if(flag==0 && flag1==1 && flag0==1)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+ 
+        
+        int diff=count0-count1;
+        if(diff>=0)
+        {
+            if((diff/2)>start0.size())
+            {
+                cout<<-1<<endl;
+                continue;
+            }
+        }
+        else
+        {
+            int lmao=diff*(-1);
+            if((lmao/2)>start1.size())
+            {
+                cout<<-1<<endl;
+                continue;
+            }
+        }
+        bool temp=0;
+        int index=0;
+        vector<int> ans;
+        while(diff>=0)
+        {
+            temp=1;
+            if(diff<=1)
+                break;
+            ans.pb(start0[index]);
+            index++;
+            diff-=2;
+        }
+        if(temp==0)
+        {
+            diff*=-1;
+            while(diff>=0)
+            {
+                if(diff<=1)
+                    break;
+                ans.pb(start1[index]);
+                index++;
+                diff-=2;
+            }
+        }
+        cout<<ans.size()<<endl;
+        for(int i=0;i<ans.size();i++)
+            cout<<ans[i]+1<<" ";
+        cout<<endl;
+        
+        
+ 
+        
+    }
+ 
+    return 0;
 }
-
