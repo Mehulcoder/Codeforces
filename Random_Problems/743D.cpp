c3f1a66164f4db27c2407fbd51e6990c273d4094
Mehulcoder <mehul355180@gmail.com>
2020-09-25 15:47:53 +0530

    Added

--- Random_Problems/743D.cpp
+++ Random_Problems/743D.cpp
@@ -29,10 +29,68 @@ const ll MOD = 1e9 + 9;
 
 ll n; vll a;
 vvll edges;
+vll sums;
+vll dp;
+bool hasTwoChild = 0;
+
+ll makeSum(ll root, ll par = -1) {
+	sums[root] = a[root];
+	trav(child, edges[root]) {
+		if (child == par) continue;
+		sums[root] += makeSum(child, root);
+	}
+
+	return sums[root];
+}
+
+void preCalcMaxi(ll root, ll par = -1) {
+	dp[root] = sums[root];
+
+	trav(child, edges[root]) {
+		if (child == par) continue;
+		preCalcMaxi(child, root);
+		dp[root] = max(dp[root], dp[child]);
+	}
+
+	return;
+}
+
+ll dfs(ll root, ll par = -1) {
+	ll ans = -INF;
+	vll temp;
+
+	if (edges[root].size() > 2) hasTwoChild = 1;
+
+	ll maxi = -INF;
+	ll sMaxi = -INF;
+	trav(child, edges[root]) {
+		if (child == par) continue;
+		if (dp[child] >= maxi) {
+			sMaxi = maxi;
+			maxi = dp[child];
+		} else {
+			sMaxi = max(dp[child], sMaxi);
+		}
+	}
+
+	if (sMaxi != -INF) {
+		ans = maxi + sMaxi;
+	}
+
+	trav(child, edges[root]) {
+		if (child == par) continue;
+		ans = max(ans, dfs(child, root));
+	}
+
+	return ans;
+}
+
 void solve() {
 	cin >> n;
 	a.resize(n, 0);
 	rep(i, n) cin >> a[i];
+	dp.resize(n + 1, -INF);
+	sums.resize(n + 1, 0);
 
 	edges.resize(n);
 	rep(i, n - 1) {
@@ -41,6 +99,16 @@ void solve() {
 		edges[a].push_back(b);
 		edges[b].push_back(a);
 	}
+
+	makeSum(0);
+	preCalcMaxi(0);
+
+	hasTwoChild = hasTwoChild || (edges[0].size() > 1);
+	ll ans = -INF;
+	ans = dfs(0);
+	if (!hasTwoChild) cout << "Impossible" << '\n';
+	else cout << ans << '\n';
+	return;
 }
 
 int main(int argc , char ** argv) {
