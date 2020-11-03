33b6fd7383fd474332efca08b1a4c876ad9c9049
Mehulcoder <mehul355180@gmail.com>
2020-11-03 21:24:41 +0530

    Removed debug

--- AtCoderDp/O.cpp
+++ AtCoderDp/O.cpp
@@ -25,45 +25,6 @@ using pll = pair<ll, ll>;
 #define trav(a, x) for(auto& a : x)
 #define fil(ar, val) memset(ar, val, sizeof(ar))
 const ll MOD = 1e9 + 7;
-#ifdef mehul
-template<typename T>
-void __p(T a) { cout << a << " "; }
-template<typename T>
-void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
-template<typename T, typename F>
-void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
-template<typename T, typename F>
-void __p(std::vector<pair<T, F>> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
-template<typename T, typename ...Arg>
-void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
-template<typename Arg1>
-void __f(const char *name, Arg1 &&arg1) {
-	cout << name << " : "; __p(arg1); cout << endl;
-}
-template<typename Arg1, typename ... Args>
-void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-	int bracket = 0, i = 0;
-	for (; ; i++)
-		if (names[i] == ',' && bracket == 0)
-			break;
-		else if (names[i] == '(')
-			bracket++;
-		else if (names[i] == ')')
-			bracket--;
-	const char *comma = names + i;
-	cout.write(names, comma - names) << " : ";
-	__p(arg1);
-	cout << "| ";
-	__f(comma + 1, args...);
-}
-#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
-int begtime = clock();
-#define end_routine() cout << "\n\nTime elapsed: "<< fixed << double(clock() - begtime)*1000/CLOCKS_PER_SEC << setprecision(12) << " ms\n\n";
-#else
-#define endl '\n'
-#define trace(...)
-#define end_routine()
-#endif
 
 const ll N = 22;
 
