afb0791469e4f54c2e05c336011616361fd3c616
Mehul Chaturvedi <mehul355180@gmail.com>
2021-06-22 01:37:26 +0530

    Added a 1600 and a 1500 level problem.

---
+++ Random_Problems/1526C.cpp
@@ -0,0 +1,68 @@
+
+/*
+Author: Mehul Chaturvedi
+Talent is overrated
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define vll vector<long long>
+#define vvll vector<vector<ll>>
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+#define INF 4557430888798830399ll
+#define fr(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define rep(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define repr(i, n) for (int i = n; i >= 0; i--)
+#define frr(i, a, b) for (int i = (a), _b = (b); i >= _b; i--)
+#define trav(a, x) for(auto& a : x)
+#define fil(ar, val) memset(ar, val, sizeof(ar))
+const ll MOD = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	vll a(n, 0);
+	rep(i, n) cin >> a[i];
+
+	multiset<ll> s;
+	ll sum = 0, ans = 0;
+	trav(elem, a) {
+		if (elem >= 0) {
+			sum += elem;
+			ans++;
+		} else if (sum + elem >= 0) {
+			sum += elem;
+			ans++;
+			s.insert(elem);
+		} else if (!s.empty() && *(s.begin()) < elem) {
+			sum -= (*s.begin());
+			s.erase(s.begin());
+
+			s.insert(elem);
+			sum += elem;
+		}
+	}
+	cout << ans << endl;
+	return;
+}
+
+int main(int argc , char ** argv) {
+	ios_base::sync_with_stdio(false) ;
+	cin.tie(NULL) ;
+
+	ll t = 1;
+
+	while (t--) {
+		solve();
+	}
+
+	return 0 ;
+}
