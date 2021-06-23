eb33fc9de35dba9854acb075ac939cd1227c9da9
Mehul Chaturvedi <mehul355180@gmail.com>
2021-06-23 22:48:29 +0530

    Added and explanation to a problem, based on number Theory.

--- Random_Problems/1514C.cpp
+++ Random_Problems/1514C.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+/**
+ * We want that the product (p) mod n = 1            --------- (1)
+ * But also we know that gcd(p, n) == gcd(p mod n, n)
+ * From equation number 1, gcd(p, n) == gcd(1, n) == 1
+ * And for gcd(p, n) to be 1, every factor of p should be coprime to n
+ * Now there is one thing that we are sure of, gcd(p, n) == 1
+ * But what if p mod n doesn't come out to be 1, it'll surely be less than n
+ * We can remove that pmodn from our list of factors for creating the p
+ * and remaining ones will be our ans
+ */
+
 bool ok[100005];
 int main() {
     int n;
